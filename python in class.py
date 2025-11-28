6bc972013e2cc9a14b7b25c50026cfda79c8275f
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-11-28 23:43:34 +0600

    A basic function programm

---
+++ python in class.py
@@ -0,0 +1,17 @@
+class Grandfather:
+    def degree(self):
+        print("Grandfather:MA passed")
+class Father:
+    def degree(self):
+        print("Father:BSC of CSE")
+class Son:
+    def degree(self):
+        print("Son:Student of Computer Science")
+
+GF=Grandfather()
+FA=Father()
+S=Son()
+GF.degree()
+FA.degree()
+S.degree()
+    
