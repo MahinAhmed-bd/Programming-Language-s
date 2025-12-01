62af6e4f84cff0741ae89e59850624fd473fd35a
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-01 23:27:40 +0600

    assignment number three

---
+++ Assignment-of-java/assignment3.java
@@ -0,0 +1,26 @@
+package com.mycompany.assignment3;
+
+/**
+ *
+ * @author Shazzad
+ */
+import java.util.Scanner;
+
+public class assignment3 {
+
+    public static void main(String[] args) {
+        int a, b, c, s;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a :");
+        a = input.nextInt();
+        System.out.println("Enter b :");
+        b = input.nextInt();
+        System.out.println("Enter c :");
+        c = input.nextInt();
+        s = ((a + b + c) / 2);
+        if (((a + b) > c) && ((b + c) > a) && ((c + a) > b)) {
+            double Area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            System.out.println("The area of triangle is :" + Area);
+        }
+    }
+}
