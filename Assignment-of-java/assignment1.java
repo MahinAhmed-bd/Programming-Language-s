8b8f7dc7fa5efb9b9a92667e2043b5ebf1f2bfbd
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-01 23:20:33 +0600

    First Assignment of java

---
+++ Assignment-of-java/assignment1.java
@@ -0,0 +1,8 @@
+package com.mycompany.assignment1;
+
+public class assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("I am studying Diploma in Engineering at AITVET.");
+    }
+}
