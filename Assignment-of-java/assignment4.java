b3363477785b576cc9ac7a51458c98097d089193
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-02 22:44:55 +0600

    Quadratic Equation

---
+++ Assignment-of-java/assignment4.java
@@ -0,0 +1,31 @@
+package com.mycompany.roots;
+
+/**
+ *
+ * @author Shazzad
+ */
+import java.util.Scanner;
+
+public class Roots {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a :");
+        double a = input.nextDouble();
+        System.out.print("Enter b :");
+        double b = input.nextDouble();
+        System.out.print("Enter c :");
+        double c = input.nextDouble();
+        double d = ((b * b) - (4 * a * c));
+        if (d > 0) {
+            double r1 = ((-b + Math.sqrt(d)) / 2 * a);
+            double r2 = ((-b - Math.sqrt(d)) / 2 * a);
+            System.out.println("The roots are :" + r1 + "and" + r2);
+        } else if (d == 0) {
+            double r = (-b / (2 * a));
+            System.out.println("The roots are :" + r);
+        } else {
+            System.out.println("Roots are imaginary. ");
+        }
+    }
+}
