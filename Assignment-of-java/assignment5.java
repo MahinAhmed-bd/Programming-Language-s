18fc2c823a78dbf345261ffa09c00a866784f8c5
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-03 22:02:42 +0600

    Writing code in assignment no4 in java

---
+++ Assignment-of-java/assignment5.java
@@ -0,0 +1,36 @@
+package com.mycompany.grade;
+
+import java.util.Scanner;
+
+public class Grade {
+
+    public static void main(String[] args) {
+        int mark;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your mark :");
+        mark = input.nextInt();
+        String grade;
+        switch (mark / 10) {
+            case 10:
+            case 9:
+            case 8:
+                grade = "A+";
+                break;
+            case 7:
+                grade = "A-";
+                break;
+            case 6:
+                grade = "B";
+                break;
+            case 5:
+                grade = "C";
+                break;
+            case 4:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+        }
+        System.out.println("The Student Garde is :" + grade);
+    }
+}
