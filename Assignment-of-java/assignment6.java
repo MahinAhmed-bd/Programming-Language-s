696ee62068425464fd9938ff22e98a007590ad5b
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-12 23:49:15 +0600

    Adding assignment no. 6

---
+++ Assignment-of-java/assignment6.java
@@ -0,0 +1,11 @@
+
+public class SeriesSum {
+
+    public static void main(String[] args) {
+        int i, sum = 0;
+        for (i = 1; i <= 99; i++) {
+            sum = sum + (i * i);
+        }
+        System.out.println("The sum of series is: " + sum);
+    }
+}
