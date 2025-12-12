94d79b02ded13cd9bb36c656809c7ad0558e8f8b
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-12 23:51:14 +0600

    Adding assignment no 7

---
+++ Assignment-of-java/assignment7.java
@@ -0,0 +1,12 @@
+
+public class printseries {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 68; i >= 28; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
