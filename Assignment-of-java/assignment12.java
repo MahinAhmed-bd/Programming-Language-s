3858a46bd5b08d30bb59af8eca71719e9505d9f0
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-13 21:34:14 +0600

    Writing code for Assignment no. 12

--- Assignment-of-java/assignment12.java
+++ Assignment-of-java/assignment12.java
@@ -1,1 +1,33 @@
 // Assignment Number 12
+
+class A {
+
+    void show1() {
+        System.out.println("It's a Super class.");
+    }
+}
+
+class B extends A {
+
+    void show2() {
+        System.out.println("It's a sub class.");
+    }
+}
+
+class C extends B {
+
+    void show3() {
+        System.out.println("It's a multilevel class.");
+    }
+}
+
+public class Multilevel {
+
+    public static void main(String[] args) {
+        C ob = new C();
+        ob.show1();
+        ob.show2();
+        ob.show3();
+    }
+
+}
