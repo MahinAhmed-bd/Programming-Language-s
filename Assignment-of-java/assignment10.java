35d5e61b2b161be5a8f09eae06ee98ab52a76d17
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-13 21:27:05 +0600

    Solving problems in assignment 10

--- Assignment-of-java/assignment10.java
+++ Assignment-of-java/assignment10.java
@@ -1,2 +1,13 @@
-for java
-      ;
+package assignment10;
+
+public class assignment10 {
+
+    String name = "Mahin Ahmed";
+    int roll = 858157;
+
+    public static void main(String[] args) {
+        Objects s = new Objects();
+        System.out.println("Student Name :" + s.name);
+        System.out.println("Student board roll :" + s.roll);
+    }
+}
