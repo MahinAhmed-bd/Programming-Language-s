44596b5bb22121b191ae4b69deeb51472b61f01e
MahinAhmed-bd <mahintaskinff@gmail.com>
2025-12-18 22:59:04 +0600

    Adding a python file

---
+++ pythonin2ndand3rd/Test.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+print("The summation of ",a," and" ,b, "is :",a+b, )
+print("The subtruction of ",a," and" ,b, "is :",a-b, )
+print("The multiple of ",a," and" ,b, "is :",a*b, ) 
+if b==0:
+    print("The division of ",a," and" ,b, "is : 0")
+else:
+    print("The division of ",a," and" ,b, "is :",a/b, )
